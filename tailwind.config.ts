c4cb195887af269826d430110059761e5ebcc2b8
thejayadad <74875595+thejayadad@users.noreply.github.com>
2025-01-06 15:52:26 -0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
+
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|card|ripple|spinner).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js"
   ],
   theme: {
     extend: {
