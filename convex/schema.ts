e2578bd76344d8b5d6e50ee981510702c7719973
thejayadad <74875595+thejayadad@users.noreply.github.com>
2025-01-08 15:38:11 -0500

    update

--- convex/schema.ts
+++ convex/schema.ts
@@ -13,6 +13,7 @@ export default defineSchema({
     userId: v.string(),
     isArchived: v.boolean(),
     content: v.optional(v.string()),
+    color: v.optional(v.string()),
     coverImage: v.optional(v.string()),
     icon: v.optional(v.string()),
     isPublished: v.boolean()
